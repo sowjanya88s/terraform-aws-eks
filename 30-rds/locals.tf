842e18594f578a808f1f6cee68b0325a62349d1c
sowjanya <devopssowjanya88s@gmail.com>
2026-06-29 21:38:19 +0530

    new

--- 30-rds/locals.tf
+++ 30-rds/locals.tf
@@ -1,6 +1,6 @@
 locals {
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
-    database_subnet_ids = data.aws_ssm_parameter.database_subnet_group_name.value
+    database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value
     common_tags = {
     Project = var.project
     Environment = var.environment

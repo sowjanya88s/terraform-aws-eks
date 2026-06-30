1bcda222620ae8733c3d61bdb1de8f06f51af500
sowjanya <devopssowjanya88s@gmail.com>
2026-06-30 21:04:05 +0530

    new

--- 50-eks/main.tf
+++ 50-eks/main.tf
@@ -33,7 +33,7 @@ module "eks" {
 
   # EKS Managed Node Group(s)
   eks_managed_node_groups = {
-    example = {
+    blue = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["m5.xlarge"]
